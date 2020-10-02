e6ce0b86634d572cae1fd67087d71d2811592def
nicholasgalante1997 <nicholasgalante1997@gmail.com>
2020-10-02 16:35:44 -0400

    nodejs:12.x

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
-// Small change to resend build with new role.
-
 function App() {
   return (
     <div className="App">
